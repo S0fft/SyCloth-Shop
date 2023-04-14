03a6d3f502bb1752e168aa1ac615c10b5b5cdd3a
Yevhen <pelikh123.evg@gmail.com>
2023-04-14 17:12:43 +0200

    new models + work with admin

--- base/urls.py
+++ base/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from products.views import index, products
 
-urlpatterns = [  # Пути доступные на сайте
+
+urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="index"),
     path("products/", products, name="products"),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=static)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,3 @@
-# Create your models here.
-# Файл для создание таблиц в БД
 from django.db import models
 
 
@@ -14,7 +12,10 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)  # Определенная строка, с определенным кол-во символов
     description = models.TextField()  # Текстовое поле, безограничения
-    price = models.DecimalField(max_digits=6, decimal_places=2)  # Цена, 6 цифр до запятой, 2 цифры после
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена, 6 цифр до запятой, 2 цифры после
     quantity = models.PositiveSmallIntegerField(default=0)  # Позитивные малые числа, по умолчанию значение 0
-    image = models.ImageField(upload_to="produsts_images")  # Поле для вставки фотографий, сохроняются в папку "products..."
+    image = models.ImageField(upload_to="products_images")  # Поле для вставки фотографий, сохроняются в папку "products..."
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Продукт: {self.name} | Категория: {self.category.name}"
