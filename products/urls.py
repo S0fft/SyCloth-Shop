d140fc57044b2dad19ffa7e3fdafddfd3773dc02
Yevhen <pelikh123.evg@gmail.com>
2023-05-26 19:06:40 +0200

    a lot of everything : )

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from products.views import products
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'),
 ]
