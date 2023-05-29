b9b47757afd2460250c3c26a660a1e851a828796
Yevhen <pelikh123.evg@gmail.com>
2023-05-29 12:06:08 +0200

    added login-controller

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,11 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput, required=False)
+
     class Meta:
         model = User
         fields = ['username', 'password']
-
