bed59898110e51c40d3af49fe7d4daad05259f0a
Yevhen <pelikh123.evg@gmail.com>
2023-06-28 09:20:03 +0200

    update: index, products views to CBV

--- base/urls.py
+++ base/urls.py
@@ -9,7 +9,7 @@ from products.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(extra_context={'title': 'Store'}), name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
 ]
