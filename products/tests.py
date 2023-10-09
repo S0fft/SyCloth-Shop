a4484d479ee89c1dacbac46a22d4f7e53ed8d0dd
Yevhen <pelikh123.evg@gmail.com>
2023-10-09 11:36:22 +0200

    added test for IndexView

--- products/tests.py
+++ products/tests.py
@@ -1,1 +1,16 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)  # Проверяем, успешно ли пользователь получает index страницу
+        print(response)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
