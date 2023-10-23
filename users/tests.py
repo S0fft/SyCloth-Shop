4b5bfc46a22d22e975c71d98b36b93ebac0c86ca
Yevhen <pelikh123.evg@gmail.com>
2023-10-23 10:31:55 +0200

    added test_user_registration_get for registraion testing

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,19 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
+
+from users.forms import UserRegistrationForm
 
 
 class UserRegistrationViewTestCase(TestCase):
 
     def setUp(self):
         self.path = reverse('users:registration')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.content_data['title'], 'SyCloth - Registration')
+        self.assertTemplateUsed(response, 'user/registration.html')
