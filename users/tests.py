28fcf0b14d60ed20ca0d9d4e2fb1e309a9ad882c
Yevhen <pelikh123.evg@gmail.com>
2023-10-23 09:58:11 +0200

    added setUp for UserRegistrationViewTestCase

--- users/tests.py
+++ users/tests.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self):
+        self.path = reverse('users:registration')
