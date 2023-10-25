84a70780aa466164359d415112febba83cbb0b0a
Yevhen <pelikh123.evg@gmail.com>
2023-10-25 08:45:34 +0200

    added annotations in common/views

--- common/views.py
+++ common/views.py
@@ -1,8 +1,8 @@
 class TitleMixin:
-    title = None
+    title: str = None
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.title
+    def get_context_data(self, **kwargs: dict[str, any]) -> dict[str, any]:
+        context: dict = super().get_context_data(**kwargs)
+        context['title']: str = self.title
 
         return context
