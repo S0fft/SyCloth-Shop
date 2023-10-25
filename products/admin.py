1273d8c95164fea5237c899b8fb77ea12448f9c0
Yevhen <pelikh123.evg@gmail.com>
2023-10-25 08:49:30 +0200

    added annotations in products/admin

--- products/admin.py
+++ products/admin.py
@@ -7,14 +7,14 @@ admin.site.register(ProductCategory)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'quantity', 'category']
-    fields = ['image', 'name', 'description', ('price', 'quantity'), 'category']
-    search_fields = ['name']
-    ordering = ['name']
+    list_display: list[str] = ['name', 'price', 'quantity', 'category']
+    fields: list[str] = ['image', 'name', 'description', ('price', 'quantity'), 'category']
+    search_fields: list[str] = ['name']
+    ordering: list[str] = ['name']
 
 
 class BasketAdmin(admin.TabularInline):
     model = Basket
-    fields = ['product', 'quantity', 'created_timestamp']
-    readonly_fields = ['created_timestamp']
-    extra = 0
+    fields: list[str] = ['product', 'quantity', 'created_timestamp']
+    readonly_fields: list[str] = ['created_timestamp']
+    extra: int = 0
