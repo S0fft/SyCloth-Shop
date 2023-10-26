6f539859d61f386a80f11e2a3a34bb1cb731e14a
Yevhen <pelikh123.evg@gmail.com>
2023-10-26 09:31:43 +0200

    added est_user_registration_post_success - returns allauth.socialaccount.models.SocialApp.DoesNotExist

--- users/tests.py
+++ users/tests.py
@@ -3,10 +3,9 @@ from http import HTTPStatus
 
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.timezone import now
 
-# from django.utils.timezone import now
-
-# from users.models import EmailVerification, User
+from users.models import EmailVerification, User
 
 
 class UserRegistrationViewTestCase(TestCase):
@@ -14,32 +13,30 @@ class UserRegistrationViewTestCase(TestCase):
     def setUp(self) -> None:
         self.data: dict[str, str] = {
             'first_name': 'name', 'last_name': 'surname',
-            'username': 'uname', 'email': 'testmail123.@gmail.com',
+            'username': 'uname', 'email': 'email123.test@gmail.com',
             'password1': '654321qQ', 'password2': '654321qQ'
         }
         self.path: str = reverse('users:registration')
 
-    def test_user_registration_get(self):
+    def test_user_registration_get(self) -> None:
         response: str = self.client.get(self.path)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         self.assertEqual(response.context_data['title'], 'SyCloth - Registration')
         self.assertTemplateUsed(response, 'users/registration.html')
 
-    # def test_user_registration_post_success(self): -- WILL BE FIXED & DONE
-    #     username = self.data['username']
-    #     self.assertFalse(User.objects.filter(username=username).exists())
+    def test_user_registration_post_success(self) -> None:
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
 
-    #     response = self.client.post(self.path, self.data)
-    #     self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
 
-    #     self.assertRedirects(response, reverse('users:login'))
-    #     self.assertTrue(User.objects.filter(username=username).exists())
-
-    #     email_verification = EmailVerification.objects.filter(user__username=username)
-    #     self.assertTrue(email_verification.exists())
-
-    #     self.assertEqual(
-    #         email_verification.first().expiration.date(),
-    #         (now() + timedelta(hours=48)).date()
-    #     )
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
