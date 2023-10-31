d2ba65d4cb56d5e3c1ae597708dda369cb095369
Yevhen <pelikh123.evg@gmail.com>
2023-10-31 14:02:39 +0100

    added annotations for celery vars

--- base/settings.py
+++ base/settings.py
@@ -161,5 +161,5 @@ SOCIALACCOUNT_PROVIDERS: dict[str, dict[list[str]]] = {
     }
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL: str = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND: str = 'redis://127.0.0.1:6379'
