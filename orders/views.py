bc81e0babde4650d04f5c4935c28295019af2066
Yevhen <pelikh123.evg@gmail.com>
2023-11-13 13:21:45 +0100

    created OrderCreateView in orders/views.py

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
 from django.views.generic.edit import CreateView
 
+from common.views import TitleMixin
+from orders.forms import OrderForm
 
-class OrderCreateView(CreateView):
+
+class OrderCreateView(TitleMixin, CreateView):
+    title = 'SyCloth - Placing an order'
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
