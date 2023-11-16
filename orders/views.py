ab22add4faf0ba6778f1a9c2c52770bffd1d4740
Yevhen <pelikh123.evg@gmail.com>
2023-11-16 10:10:28 +0100

    added logic for order create

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from common.views import TitleMixin
@@ -8,3 +9,9 @@ class OrderCreateView(TitleMixin, CreateView):
     title: str = 'SyCloth - Placing an order'
     template_name: str = 'orders/order-create.html'
     form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+
+        return super().form_valid(form)
