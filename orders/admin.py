785ceca7f68b5befdca528342438b5698527f025
Yevhen <pelikh123.evg@gmail.com>
2023-11-29 16:59:36 +0100

    added created info to list_display

--- orders/admin.py
+++ orders/admin.py
@@ -5,11 +5,11 @@ from orders.models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'status')
-    fields = (
+    list_display: list[str] = ('__str__', 'status', 'created')
+    fields: list[str] = (
         'id', 'created',
         ('first_name', 'last_name'),
         ('email', 'address'),
         'basket_history', 'status', 'initiator',
     )
-    readonly_fields = ('id', 'created')
+    readonly_fields: list[str] = ('id', 'created')
