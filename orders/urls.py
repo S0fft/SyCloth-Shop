da2a8693053465cee61591702c28128178018236
Yevhen <pelikh123.evg@gmail.com>
2024-01-26 12:52:57 +0100

    added route to oders list

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from orders.views import CanceledTemplateView, OrderCreateView, SuccessTemplateView
+from orders import views
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('order-create/', OrderCreateView.as_view(), name='order_create'),
-    path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
-    path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
+    path('', views.OrderListView.as_view(), name='orders_list'),
+    path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
+    path('order-canceled/', views.CanceledTemplateView.as_view(), name='order_canceled'),
 ]
