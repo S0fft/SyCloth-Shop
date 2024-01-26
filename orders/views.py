8fd8a450d34dcd03a08693b95a9134433f363a3c
Yevhen <pelikh123.evg@gmail.com>
2024-01-26 13:40:48 +0100

    added logic for order view

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Any
 
 import stripe
 from django.conf import settings
@@ -6,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
@@ -37,6 +39,17 @@ class OrderListView(TitleMixin, ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'SyCloth - Order № {self.object.id}'
+
+        return context
 
 
 class OrderCreateView(TitleMixin, CreateView):
