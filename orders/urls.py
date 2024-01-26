00c92f3ca123f36c6baf48a6b282c7e59305185d
Yevhen <pelikh123.evg@gmail.com>
2024-01-26 13:39:54 +0100

    added route to order view

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,7 @@ app_name = 'orders'
 
 urlpatterns = [
     path('', views.OrderListView.as_view(), name='orders_list'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order'),
     path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
     path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
     path('order-canceled/', views.CanceledTemplateView.as_view(), name='order_canceled'),
