e032f2754dbf9bdc514b5670bca97ff6a9b3fa9a
Yevhen <pelikh123.evg@gmail.com>
2024-02-01 10:39:55 +0100

    added dynamic routes for products API

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from api.views import ProductListAPIView
+from api.views import ProductModelViewSet
 
 app_name: str = 'api'
 
+router = routers.DefaultRouter()
+router.register(r'products', ProductModelViewSet)
+
 urlpatterns = [
-    path('product-list/', ProductListAPIView.as_view(), name='product_list'),
+    path('', include(router.urls)),
 ]
